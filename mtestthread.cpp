f2c4c6c52a38a1e7730a3b6793aa7f9f430ff070
posproger <posproger@gmail.com>
2014-12-29 20:29:41 +0300

    Для тестов

--- mtestthread.cpp
+++ mtestthread.cpp
@@ -3,7 +3,7 @@
 
 MTestThread::MTestThread(qintptr socketDescriptor) {
     m_socketDescriptor = socketDescriptor;
-    m_netManager = new QLClientNetManager(0, 0);
+    m_netManager = new QLClientNetManager(0, 0, this);
     connect(m_netManager,SIGNAL(newBinaryMessageReceived(QByteArray)),this,SLOT(newBinMsgFromClient(QByteArray)));
     connect(m_netManager,SIGNAL(socketDisconnected()),this,SLOT(socketDisconnected()));
     connect(m_netManager,SIGNAL(binaryMessageSent(quint32)),this,SLOT(binaryMessageSent(quint32)));
@@ -35,16 +35,26 @@ void MTestThread::newBinMsgFromClient(QByteArray in) {
     ++m_count;
     QString zz;
     zz.append(in);
+
+    QByteArray zz1;
+    for ( int i=0; i<60000; ++i ) {
+        zz1.append("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\r\n");
+    }
+
     if ( in.size()!=zz.size() ) {
         ++m_err;
         qlDebug() << "Size incorrect!" << in.size() << zz.size() << m_err;
-        QByteArray zz1;
-        for ( int i=0; i<60000; ++i ) {
-            zz1.append("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\r\n");
-        }
+//        QByteArray zz1;
+//        for ( int i=0; i<60000; ++i ) {
+//            zz1.append("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\r\n");
+//        }
         m_netManager->sendBinaryMessage(zz1, m_packetN++);
     } else {
         m_netManager->sendBinaryMessage(in, m_packetN++);
+    }
+    if ( in != zz1) {
+        ++m_err;
+        qlDebug() << "Packet incorrect!" << qChecksum(in.constData(),in.size()) << qChecksum(zz1.constData(),zz1.size()) << m_err;
     }
 }
 
@@ -57,6 +67,6 @@ void MTestThread::checkStatistics(void) {
 }
 
 void MTestThread::binaryMessageSent(quint32 packetN) {
-    qlDebug() << packetN;
+    qlDebug() << packetN << QDateTime::currentDateTime();
 }
 
